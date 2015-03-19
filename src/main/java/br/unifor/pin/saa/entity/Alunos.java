81c50ab54ecb2ef267f464f2f91563501b65ee22
Adriano Patrick Cunha <adrianopatrick@gmail.com>
2015-03-19 19:03:13 -0300

    [patrick.cunha] - criado classes da camada de acesso a dados

--- src/main/java/br/unifor/pin/saa/entity/Alunos.java
+++ src/main/java/br/unifor/pin/saa/entity/Alunos.java
@@ -12,7 +12,6 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="alunos")
-
 public class Alunos implements Serializable{
 
 
--- src/test/java/br/unifor/pin/saa/dao/UsuarioDAOTest.java
+++ src/test/java/br/unifor/pin/saa/dao/UsuarioDAOTest.java
@@ -3,7 +3,6 @@ package br.unifor.pin.saa.dao;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +18,7 @@ public class UsuarioDAOTest {
 	@Autowired
 	private UsuarioDAO usuarioDAO;
 
-	@Ignore
+	@Test
 	public void testSalvar() throws Exception {
 		
 		Usuarios usuario = new Usuarios();
